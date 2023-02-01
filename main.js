a99e377be44a24ecab12c4a1ef3a74ef15869ba6
V0rTeXGG <122302365+V0rTeXGG@users.noreply.github.com>
2023-02-01 16:05:51 +0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-    $(".bars").click(function(){
-        $(".bars").toggleClass("active")
-        $(".nav-list-block").toggleClass("active")
-        $("body").toggleClass("hiden")
-    })
-})
